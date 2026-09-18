00d81d36f9abba617c9d34cc94895ad7d809ef4f
Victor Baccon Vieira <victorbacconvieira@gmail.com>
2026-09-18 17:34:20 +0000

    add: Definição das cores utilizadas no app

--- camisa10App/tailwind.config.js
+++ camisa10App/tailwind.config.js
@@ -8,7 +8,32 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'verde': {
+          DEFAULT: '#00C853',
+          claro: '#39FF14',
+          escuro: '#007A35'
+        },
+
+        'texto': {
+          DEFAULT: '#F5F7F8',
+          sec: '#A8B2B9',
+          ter: '#6F7B83'
+        },
+
+        'cartao': {
+          DEFAULT: '#16232D',
+          escuro: '#111D27',
+          borda: '#26343E'
+        },
+
+        'fundo': '#071018',
+        'vermelho': '#E53935',
+        'amarelo': '#FFC107'
+      },
+
+    },
   },
 
   plugins: [],
